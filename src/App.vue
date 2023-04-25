fb647da961966fa9cb2013c853cc8555c2786303
john <jjmiyao@gmail.com>
2023-04-24 23:33:46 -0700

    Added boilerplate css files

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@
 
 <template>
    <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
       <div class="wrapper">
          <HelloWorld msg="Hi John!" />
 
